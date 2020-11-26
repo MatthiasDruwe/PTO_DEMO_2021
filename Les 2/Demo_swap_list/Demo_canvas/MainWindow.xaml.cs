d4315f869b8ba409826f49ffbcf9870bded722e4
Matthias Druwé <matthias.druwe@odisee.be>
2020-11-26 20:05:33 +0100

    Draw bear

---
+++ Les 2/Demo_swap_list/Demo_canvas/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo_canvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DrawBear(200, 120);
+            DrawBear(400, 120);
+        }
+
+
+        private void DrawBear(double x, double y)
+        {
+            // left ear
+            DrawEar(x-60, y-60);
+            // right ear
+            DrawEar(x+60, y-60);
+            // Face
+            DrawFace(x, y);
+            // Eye
+            DrawEye(x-30, y-25);
+            DrawEye(x+30, y-25);
+            // Mouth
+            DrawMouth(x,y+10);
+            // nose
+            DrawNose(x,y);
+
+        }
+
+        private void DrawMouth(double x, double y)
+        {
+            AddCircle(x, y, 120, 80, Brushes.Beige);
+        }
+
+        private void DrawEye(double x, double y)
+        {
+            AddCircle(x, y, 20, 40, Brushes.White);
+            AddCircle(x, y-5, 7.5, 7.5, Brushes.Black);
+        }
+
+        private void DrawFace(double x, double y)
+        {
+            AddCircle(x, y, 200, 160, Brushes.SaddleBrown);
+        }
+
+        private void DrawNose(double x, double y)
+        {
+            AddCircle(x, y, 20, 20, Brushes.Red);
+
+        }
+
+        private void DrawEar(double x, double y)
+        {
+            AddCircle(x, y, 80, 80, Brushes.SaddleBrown);
+            AddCircle(x, y, 40, 40, Brushes.Beige);
+        }
+
+        private void AddCircle(double x, double y, double width, double height, Brush color)
+        {
+            Ellipse circle = new Ellipse();
+            circle.Height = height;
+            circle.Width = width;
+            circle.Fill = color;
+            circle.Stroke = Brushes.Black;
+            circle.StrokeThickness = 2.0;
+            Canvas.SetLeft(circle, x - width/2); // position based on center
+            Canvas.SetTop(circle, y - height/2); // position based on center
+            drawingCanvas.Children.Add(circle);
+        }
+    }
+}
---
+++ Les 2/Demo_swap_list/Demo_swap_list/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_swap_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Create list with hardcoded data
+            List<int> nummers = new List<int>();
+            nummers.Add(1);
+            nummers.Add(2);
+            nummers.Add(3);
+            nummers.Add(4);
+
+            PrintList(nummers);
+
+
+            // Swap 2 numbers
+            int temp = nummers[1];
+            nummers[1] = nummers[2];
+            nummers[2] = temp;
+
+            PrintList(nummers);
+
+            // To top 
+            temp = nummers[3];
+            nummers.RemoveAt(3);
+            nummers.Insert(0, temp);
+
+            PrintList(nummers);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintList(List<int> nummers)
+        {
+            foreach (int nummer in nummers)
+            {
+                Console.WriteLine(nummer);
+            }
+            Console.WriteLine("------");
+        }
+    }
+}
