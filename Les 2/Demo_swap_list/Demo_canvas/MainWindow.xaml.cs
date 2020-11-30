eca7adb5626fad2b104f3d14bda79afc835b8474
Matthias Druwé <matthias.druwe@odisee.be>
2020-11-30 18:07:15 +0100

    Add timer + click events

--- Les 2/Demo_swap_list/Demo_canvas/MainWindow.xaml.cs
+++ Les 2/Demo_swap_list/Demo_canvas/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Demo_canvas
 {
@@ -20,30 +12,51 @@ namespace Demo_canvas
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int score = 0;
+        double interval = 1000;
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
 
             DrawBear(200, 120);
-            DrawBear(400, 120);
+            SetupTimer();
+            scoreLabel.Content = score;
         }
 
+        private void SetupTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(interval); // new TimeSpan(0, 0, 5);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            drawingCanvas.Children.Clear();
+            Random random = new Random();
+            int x = random.Next(Convert.ToInt32(drawingCanvas.ActualWidth));
+            int y = random.Next(Convert.ToInt32(drawingCanvas.ActualHeight));
+            DrawBear(x, y);
+        }
 
         private void DrawBear(double x, double y)
         {
             // left ear
-            DrawEar(x-60, y-60);
+            DrawEar(x - 60, y - 60);
             // right ear
-            DrawEar(x+60, y-60);
+            DrawEar(x + 60, y - 60);
             // Face
             DrawFace(x, y);
             // Eye
-            DrawEye(x-30, y-25);
-            DrawEye(x+30, y-25);
+            DrawEye(x - 30, y - 25);
+            DrawEye(x + 30, y - 25);
             // Mouth
-            DrawMouth(x,y+10);
+            DrawMouth(x, y + 10);
             // nose
-            DrawNose(x,y);
+            DrawNose(x, y);
 
         }
 
@@ -55,7 +68,7 @@ namespace Demo_canvas
         private void DrawEye(double x, double y)
         {
             AddCircle(x, y, 20, 40, Brushes.White);
-            AddCircle(x, y-5, 7.5, 7.5, Brushes.Black);
+            AddCircle(x, y - 5, 7.5, 7.5, Brushes.Black);
         }
 
         private void DrawFace(double x, double y)
@@ -83,9 +96,23 @@ namespace Demo_canvas
             circle.Fill = color;
             circle.Stroke = Brushes.Black;
             circle.StrokeThickness = 2.0;
-            Canvas.SetLeft(circle, x - width/2); // position based on center
-            Canvas.SetTop(circle, y - height/2); // position based on center
+            Canvas.SetLeft(circle, x - width / 2); // position based on center
+            Canvas.SetTop(circle, y - height / 2); // position based on center
             drawingCanvas.Children.Add(circle);
+            circle.MouseDown += Circle_MouseDown;
+        }
+
+        private void Circle_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            drawingCanvas.Children.Clear();
+            score++;
+            scoreLabel.Content = score;
+
+            if (score % 5 == 0)
+            {
+                interval = interval * 0.9;
+                timer.Interval = TimeSpan.FromMilliseconds(interval);
+            }
         }
     }
 }
---
+++ Les 3/EasyBank/EasyBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
