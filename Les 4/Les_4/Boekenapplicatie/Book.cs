be3ad46b1e98d0fbf8169948df961028aa5c1087
Matthias Druwé <matthias.druwe@odisee.be>
2020-12-03 19:48:47 +0100

    Add boekendemo

---
+++ Les 4/Les_4/Boekenapplicatie/Book.cs
@@ -0,0 +1,74 @@
+﻿namespace Boekenapplicatie
+{
+    class Book
+    {
+        // Private members / class attributes / class variables
+        private string title;
+        private string author;
+        private int totalNumberOfPages;
+        private int currentPageReading;
+        private string reader;
+
+
+        // Constructor 
+        public Book(string title, string author, int totalNumberOfPages)
+        {
+            this.title = title;
+            this.author = author;
+            this.totalNumberOfPages = totalNumberOfPages;
+            this.currentPageReading = 0;
+        }
+
+
+        public Book(string title, string author, int totalNumberOfPages, string reader)
+        {
+            this.title = title;
+            this.author = author;
+            this.totalNumberOfPages = totalNumberOfPages;
+            this.currentPageReading = 0;
+            this.reader = reader;
+        }
+
+        // Properties / getters & setters
+
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+        }
+
+        public int TotalNumberOfPages
+        {
+            get { return totalNumberOfPages; }
+        }
+
+        public string Reader
+        {
+            get { return reader; }
+            set { reader = value; }
+        }
+
+
+        // Methodes (public & private)
+        public string NextPage()
+        {
+            if (currentPageReading < totalNumberOfPages)
+            {
+                currentPageReading++;
+                return $"Pagina {currentPageReading} van de {totalNumberOfPages} in het boek {title}";
+            }
+
+            return "Einde van het boek!";
+        }
+
+        public string ToString()
+        {
+            return $"{title} - {author}"; 
+        }
+
+    }
+}
---
+++ Les 4/Les_4/Boekenapplicatie/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boekenapplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myFirstBook = new Book("De Kleine Prins", "Blabla", 10);
+            Book mySecondBook = new Book("Harry Potter", "Blabla", 10);
+
+            List<Book> books = new List<Book>();
+            books.Add(myFirstBook);
+            books.Add(mySecondBook);
+
+            Console.WriteLine($"Ik lees momenteel \"{myFirstBook.ToString()}\"");
+
+
+            Console.WriteLine($"{myFirstBook.Reader} leest het boek");
+
+            myFirstBook.Reader = "Matthias";
+
+            Console.WriteLine($"{myFirstBook.Reader} leest het boek");
+
+
+            while (true)
+            {
+                char c = Console.ReadKey().KeyChar;
+                Console.Clear();
+                if (c == '1')
+                {
+                    Console.WriteLine(myFirstBook.NextPage());
+                }
+                else if (c == '2')
+                {
+                    Console.WriteLine(mySecondBook.NextPage());
+                }
+            }
+        }
+    }
+}
