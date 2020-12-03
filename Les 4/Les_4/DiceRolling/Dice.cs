9ff5d6f62bfad8b6ac4069d3b39ba8ad13d358c2
Matthias Druwé <matthias.druwe@odisee.be>
2020-12-03 21:17:55 +0100

    Add dice

---
+++ Les 4/Les_4/DiceRolling/Dice.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace DiceRolling
+{
+    /// <summary>
+    /// Dit is een dobbelsteen klasse die een random getal tussen 1 en 6 terug geeft.
+    /// </summary>
+    class Dice
+    {
+        // Private members / class attributes / class variables
+        private int rolledNumber;
+        private BitmapImage image;
+        private string name;
+
+        // Constructor 
+        public Dice(string name)
+        {
+            this.name = name;
+        }
+
+        public Dice()
+        {
+
+        }
+
+        // Properties / getters & setters
+        public int RolledNumber
+        {
+            get { return rolledNumber; }
+        }
+
+        public BitmapImage Image
+        {
+            get { return image; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Methodes (public & private)
+        public void Roll()
+        {
+            Random random = new Random();
+            int newNumber;
+            do
+            {
+                newNumber = random.Next(1, 7);
+
+            } while (newNumber == rolledNumber);
+            rolledNumber = newNumber;
+
+            SetImage(rolledNumber);
+        }
+
+        private void SetImage(int number)
+        {
+            string url = "";
+
+            switch (number)
+            {
+                case 1:
+                    url = "/Images/Alea_1.png";
+                    break;
+                case 2:
+                    url = "/Images/Alea_2.png";
+                    break;
+                case 3:
+                    url = "/Images/Alea_3.png";
+                    break;
+                case 4:
+                    url = "/Images/Alea_4.png";
+                    break;
+                case 5:
+                    url = "/Images/Alea_5.png";
+                    break;
+                case 6:
+                    url = "/Images/Alea_6.png";
+                    break;
+            }
+
+            if (url != "")
+            {
+                Uri resourceUri = new Uri(url, UriKind.Relative);
+                image = new BitmapImage(resourceUri);
+            }
+        }
+    }
+}
---
+++ Les 4/Les_4/DiceRolling/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiceRolling
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dice dice1 = new Dice("Dobbelsteen 1");
+        Dice dice2 = new Dice("Dobbelsteen 2");
+        Dice dice3 = new Dice("Dobbelsteen 3");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            rollDice(dice1, rolledNumber1Image, rolledNumber1Label);
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            rollDice(dice2, rolledNumber2Image, rolledNumber2Label);
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            rollDice(dice3, rolledNumber3Image, rolledNumber3Label);
+        }
+
+        private void rollDice(Dice dice, Image image, Label label)
+        {
+            dice.Roll();
+            image.Source = dice.Image;
+            label.Content = $"{dice.Name}: {dice.RolledNumber}";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rollDice(dice1, rolledNumber1Image, rolledNumber1Label);
+            rollDice(dice2, rolledNumber2Image, rolledNumber2Label);
+            rollDice(dice3, rolledNumber3Image, rolledNumber3Label);
+        }
+    }
+}
